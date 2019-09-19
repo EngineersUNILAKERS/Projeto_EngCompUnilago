d7155313faa36992f3309effa45377b782a39492
Liisted <patrick.lopes2@gmail.com>
2019-09-19 10:38:50 -0300

    Test 1 ok test FAIL

--- DB/db.php
+++ DB/db.php
@@ -3,33 +3,54 @@
 function db_connect()
 {
 
-    
-    //$mysql_hostname = 'databases-auth.000webhost.com';
-    //    $mysql_username = 'id10869503_root';
-    ///  $mysql_password = 'root123';
-    //$mysql_dbname= 'id10869503_web_dev';
-    // definições de host, database, usuário e senha
-    $host = "localhost";
-    $db   = "id10869503_web_dev";
-    $userdb= "id10869503_root";
-    $pass = "root123";
-    // conecta ao banco de dados
-    $con = mysql_pconnect($host, $userdb, $pass) or trigger_error(mysql_error(),E_USER_ERROR); 
-    // seleciona a base de dados em que vamos trabalhar
-    mysql_select_db($db, $con);
-    // cria a instrução SQL que vai selecionar os dados
-    $query = sprintf("SELECT  * FROM users");
-    // executa a query
-    $dados = mysql_query($query, $con) or die(mysql_error());
-    // transforma os dados em um array
-    $linha = mysql_fetch_assoc($dados);
-    // calcula quantos dados retornaram
-    $total = mysql_num_rows($dados);
-    
-  
-    
+    /*** mysql_hostname 
+     * 
+     * Coloque nesta variável o endereço do servidor
+     * 
+     * ***/
+    $mysql_hostname = "";
 
-    
+    /*** mysql_username 
+     * 
+     * Coloque nesta variável o user do banco de dados
+     * 
+     * ***/
+    $mysql_username = "";
+
+    /*** mysql_password 
+     * 
+     * Coloque nesta variável o password do banco de dados
+     * 
+     * ***/
+    $mysql_password = "";
+
+    /*** mysql_database 
+     * 
+     * Coloque nesta variável o nome da base de dados
+     * 
+     * ***/
+    $mysql_dbname = "";
+
+    $mysql_hostname = 'localhost';
+    $mysql_username = 'id10869503_root';
+    $mysql_password = 'root123';
+    $mysql_dbname= 'id10869503_web_dev';
+
+    try {
+        $dbh = new pdo("mysql:host=$mysql_hostname;dbname=$mysql_dbname", $mysql_username, $mysql_password);
+        /*** $message = a message saying we have connected ***/
+
+        // /*** set the error mode to exceptions ***/
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        return $dbh;
+    } 
+    catch (Exception $e) 
+    {
+        trigger_error("Erro de conexão no banco de dados <br>". $e);
+        return null;
+    }
+
 }
 
 function checkLogin($user, $MD5_password)
@@ -66,5 +87,3 @@ function checkLogin($user, $MD5_password)
         return null;
     }
 }
-
-?>
