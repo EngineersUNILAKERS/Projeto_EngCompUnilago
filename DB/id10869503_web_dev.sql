60132d8ae86eb64d75f2165382fd09541f7c335a
Liisted <patrick.lopes2@gmail.com>
2019-09-19 12:02:28 -0300

    slq db

--- DB/id10869503_web_dev.sql
+++ DB/id10869503_web_dev.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.15.10
--- https://www.phpmyadmin.net
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
 --
--- Host: upwifi-rds01.csa2zgnd6x8i.sa-east-1.rds.amazonaws.com
--- Generation Time: 17-Set-2019 às 17:43
--- Versão do servidor: 5.7.22-log
--- PHP Version: 5.6.40
+-- Host: localhost:3306
+-- Generation Time: 19-Set-2019 às 15:01
+-- Versão do servidor: 10.3.16-MariaDB
+-- versão do PHP: 7.3.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -19,8 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `id10869503_web_dev`
 --
-CREATE DATABASE IF NOT EXISTS `id10869503_web_dev` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `id10869503_web_dev`;
 
 -- --------------------------------------------------------
 
@@ -28,9 +28,9 @@ USE `id10869503_web_dev`;
 -- Estrutura da tabela `groups`
 --
 
-CREATE TABLE IF NOT EXISTS `groups` (
+CREATE TABLE `groups` (
   `id` int(11) NOT NULL,
-  `name` int(11) NOT NULL
+  `name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -38,8 +38,26 @@ CREATE TABLE IF NOT EXISTS `groups` (
 --
 
 INSERT INTO `groups` (`id`, `name`) VALUES
-(1, 0),
-(2, 0);
+(1, 'comum'),
+(2, 'administrador');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `PRODUCTS`
+--
+
+CREATE TABLE `PRODUCTS` (
+  `PRO_Id` int(50) NOT NULL,
+  `PRO_Codigo` int(50) DEFAULT NULL,
+  `PRO_Nome` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `PRO_Descricao` varchar(300) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `PRO_Categoria_Id` int(10) DEFAULT NULL,
+  `PRO_Foto` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `PRO_Preco` float(5,2) DEFAULT NULL,
+  `PRO_Estoque` float(5,4) DEFAULT NULL,
+  `PRO_Ativo` varchar(5) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -47,7 +65,7 @@ INSERT INTO `groups` (`id`, `name`) VALUES
 -- Estrutura da tabela `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `user` varchar(30) NOT NULL,
   `password` varchar(64) NOT NULL,
@@ -60,7 +78,8 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 INSERT INTO `users` (`id`, `user`, `password`, `email`, `groupId`) VALUES
-(0, 'usuario', '7c12772809c1c0c3deda6103b10fdfa0', 'user@email.com', 2);
+(0, 'usuario', '7c12772809c1c0c3deda6103b10fdfa0', 'user@email.com', 2),
+(1, 'teste', 'e807f1fcf82d132f9bb018ca6738a19f', 'teste@teste', 1);
 
 --
 -- Indexes for dumped tables
@@ -71,6 +90,12 @@ INSERT INTO `users` (`id`, `user`, `password`, `email`, `groupId`) VALUES
 --
 ALTER TABLE `groups`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `PRODUCTS`
+--
+ALTER TABLE `PRODUCTS`
+  ADD PRIMARY KEY (`PRO_Id`);
 
 --
 -- Indexes for table `users`
@@ -88,6 +113,7 @@ ALTER TABLE `users`
 --
 ALTER TABLE `users`
   ADD CONSTRAINT `user_group` FOREIGN KEY (`groupId`) REFERENCES `groups` (`id`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
