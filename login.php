aa8aefbe479ab88eb39ce1b54677734ff3495343
overclock93 <paulo.amorimh@gmail.com>
2019-09-24 16:02:48 -0300

    Update login.php

--- login.php
+++ login.php
@@ -8,9 +8,19 @@ $ConsultasBanco->Conectarbanco();
 $email = $_POST['email'];
 $senha = $_POST['senha'];
 
-$sql = "SELECT email FROM users = '$email'";
+$sql = "SELECT email FROM users WHERE email = '$email'";
 $buscar = mysqli_query($ConsultasBanco,$sql);
 
 $total = mysqli_num_rows($buscar);
 
+if($total > 0){
+    echo 'conferir senha';
+} else {
+    header('Location:index.html');
+    
+}
+
 ?>
+<script>
+    alert("Você não possui cadastro!!");
+</script>
