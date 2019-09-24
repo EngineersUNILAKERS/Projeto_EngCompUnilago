b245568910467c8dbd699a1b8eba935e793fe1a8
rafael <guimaraescorrea437@gmail.com>
2019-09-24 16:12:22 -0300

    Login 4

--- cadastro.php
+++ cadastro.php
@@ -1,37 +1,24 @@
 <?php 
 
 require('../config.php');
+
+$con=mysqli_connect($NomeDoServer,$UserDoServer,$senha,$nomeDoBanco); 
+
 $user = $_POST['usuario'];
-$MD5_password = MD5($_POST['senha']);
+$password = MD5($_POST['senha']);
 $email = $_POST['email'];
-$query_select = "SELECT email FROM usuarios WHERE user = '$email";
-$select = mysql_query($query_select,$connect);
-$array = mysql_fetch_array($select);
-$logarray = $array['email'];
- 
-  
- 
-    
-      if($logarray == $email){
- 
-        echo"<script language='javascript' type='text/javascript'>
-        alert('Esse email já existe');window.location.href='
-        cadastro.html';</script>";
-        die();
- 
-      }else{
-        $query = "INSERT INTO usuarios (user,password,email) VALUES ('$user','$password',$email)";
-        $insert = mysql_query($query,$connect);
-         
-        if($insert){
-          echo"<script language='javascript' type='text/javascript'>
-          alert('Usuário cadastrado com sucesso!');window.location.
-          href='login.html'</script>";
-        }else{
-          echo"<script language='javascript' type='text/javascript'>
-          alert('Não foi possível cadastrar esse usuário');window.location
-          .href='cadastro.html'</script>";
-        }
-      }
-    
+
+
+$select = mysql_select_db("server") or die("Sem acesso ao DB");
+$result = mysql_query("SELECT * FROM `USUARIO` WHERE `NOME` = '$login' AND `SENHA`= '$senha'");
+
+if(mysql_num_rows ($result) > 0 )
+{
+    echo 'Usuário já existe';
+
+}
+
+else{
+    $sql = "INSERT INTO users(user, senha, email) VALUES ('$user','$password','$email')";
+}
 ?>
