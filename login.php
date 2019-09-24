b8ee0ddffb23dc32ecadabd5674e6efca25ffc37
vphyre <vinipaulnic@hotmail.com>
2019-09-24 17:48:48 -0300

    Ajustando o login

--- login.php
+++ login.php
@@ -1,22 +1,28 @@
 <?php
- 
+
+//Requisitando o arquivo que contem a função de conectar com o banco
 require('ConsultasSql.php');
 
 $ConsultasBanco = new ConsultasBanco();
 $ConsultasBanco->Conectarbanco();
-
+//Variaveis que recebem o email e senha do index.html
 $email = $_POST['email'];
-$senha = $_POST['senha'];
+//Criptografando a senha com md5
+$senha = md5($_POST['senha']);
 
-$sql = "SELECT email FROM users WHERE email = '$email'";
-$buscar = mysqli_query($ConsultasBanco,$sql);
-
+//Select que retorna uma linha com email, quando a consulta achar email e senha iguais ao informado pelo user
+$sql = "SELECT email FROM users WHERE email = '$email' AND senha = '$senha'";
+$buscar = mysqli_query($ConsultasBanco->Conectarbanco(),$sql);
+//Função que indica quantas linhas o select retornou
 $total = mysqli_num_rows($buscar);
 
-if($total > 0){
-    echo 'conferir senha';
+//Se o numero de linhas seja maior que 1 ele irá redirecionar o usuário para o Dashboard, senão ele retornará mensagens de erro 
+if($total > 0)
+{
+    header('Location:Dashboard.html');
+    
 } else {
-    header('Location:index.html');
+    echo 'conferir senha';
     
 }
 
