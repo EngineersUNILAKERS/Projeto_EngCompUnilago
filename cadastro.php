1daaccaf39eff14ea7a887e46d1271d1835e8b6c
guimaraescorrea437@gmail.com <guimaraescorrea437@gmail.com>
2019-10-01 13:36:35 -0300

    Cadastro Final

--- cadastro.php
+++ cadastro.php
@@ -1,30 +1,66 @@
 <?php 
 
-require('ConsultasSql.php');
-$ConsultasBanco = new ConsultasBanco();
-$ConsultasBanco->Conectarbanco();
+//require('ConsultasSql.php');
+//$ConsultasBanco = new ConsultasBanco();
+//$ConsultasBanco->Conectarbanco();
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "englakersdb";
+
 
 $Usuario=$_POST['usuario'];
-$Email=$_POST['email'];
+$Email=$_POST['email'];	
 $Senha=MD5($_POST['senha']);
+$Grupo_Id = ($_POST['grupo']);) 
 
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
+$sql1 = "SELECT usuario FROM users WHERE usuario = '$Usuario'";
+$buscar_user = mysqli_query($conn,$sql1);
+$total = mysqli_num_rows($buscar_user);
+echo ($total);
 
-/*$result = ("SELECT * FROM users WHERE Nome = '$Usuario' AND Senha = '$Senha'");
-
-if(($result) > 0 )
+if(($total) > 0 )
 {
     echo 'Usuário já existe';
 
+}
+
+$sql = "SELECT email FROM users WHERE email = '$Email'";
+$buscar_email = mysqli_query($conn,$sql);
+$total1 = mysqli_num_rows($buscar_email);
+
+if(($total1) > 0){
+	echo 'Email já existe';
+}
+else{
+	$sql = "INSERT INTO users(usuario, email, senha) VALUES ('$Usuario', '$Email', '$Senha',$Grupo_Id)";
+	
+	$conn->query($sql);
+	echo 'Usuário cadastrado com sucesso!';
+}
+/*if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
 }*/
-/*else{*/
-    $sql = "INSERT INTO users(Nome, Senha, Email) VALUES ('$Usuario', '$Senha', '$Email')";
+
+
+/*else{a*/
+   // $sql = "INSERT INTO users(usuario, emai, senha) VALUES ('$Usuario', '$Email', '$Senha')";
     
-	if ($ConsultasBanco->ConectarBanco()->query($sql) === TRUE) {
-		echo 'Usuario criado com sucesso!<br>';
-	  }
-	  else {
-	   echo 'Error: '. $ConsultasBanco->ConectarBanco()->error;
-	  }
+	//if ($ConsultasBanco->ConectarBanco()->query($sql) === TRUE) {
+	//	echo 'Usuario criado com sucesso!<br>';
+	  //}
+	  //else {
+	  // echo 'Error:db '. $ConsultasBanco->ConectarBanco()->error;
+	 // }
 /*}*/
+$conn->close();
+
 ?>
