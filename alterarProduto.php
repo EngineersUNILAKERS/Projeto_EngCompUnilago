da6429375caa3623d586fb928acbcfa75731e21a
Junior93F <jr93flavio@gmail.com>
2019-09-30 23:58:18 -0300

    Alterar Produtos

--- alterarProduto.php
+++ alterarProduto.php
@@ -7,14 +7,14 @@ $id_prod = $_GET['id'];
 $recebe_produto = $_POST['Produto'];
 $recebe_descricao = $_POST['Descricao'];
 $recebe_categoria = $_POST['Categoria'];
-$recebe_foto1 = $_FILES['Foto'];
+/*$recebe_foto1 = $_FILES['Foto'];*/
 $recebe_preco = $_POST['dinheiro'];
 $recebe_estoque = $_POST['Estoque'];
 
 $destino = "upload/";
 
 
-if (!empty($recebe_foto1['name'])) {
+/*if (!empty($recebe_foto1['name'])) {
 
 	preg_match("/\.(jpg|jpeg|png|gif){1}$/i",$recebe_foto1['name'],$extencao1);
 	$img_nome1 = md5(uniqid(time())).".".$extencao1[1];
@@ -22,20 +22,19 @@ if (!empty($recebe_foto1['name'])) {
 	$upload_foto1=1;
 
 }
-
+PRO_Foto = '$img_nome1',
 else {
 	
-	$img_nome1=$exibe['Foto'];
+	$img_nome1=$recebe_foto1;
 	$upload_foto1=0;
 	
-}
+}*/
 
     $sql="UPDATE products SET
           PRO_Nome = '$recebe_produto',
 	      PRO_Descricao = '$recebe_descricao',
 	      PRO_Categoria_Id = '$recebe_categoria',
-          PRO_Foto = '$img_nome1',
-	      PRO_Preco = '$recebe_preco',
+          PRO_Preco = '$recebe_preco',
 	      PRO_Estoque = '$recebe_estoque',
 		  WHERE PRO_Id = '$id_prod'";
 	
