a141b0f0389a649765eb78af648695d5daa5b215
vphyre <vinipaulnic@hotmail.com>
2019-10-01 16:21:35 -0300

    Arrumando o cadastro

--- cadastro.php
+++ cadastro.php
@@ -13,36 +13,46 @@ $dbname = "englakersdb";
 $Usuario=$_POST['usuario'];
 $Email=$_POST['email'];	
 $Senha=MD5($_POST['senha']);
-$Grupo_Id = ($_POST['grupo']);) 
+$Grupo_Id = ($_POST['grupo']);
 
 $conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
+if ($conn->connect_error) 
+{
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql1 = "SELECT usuario FROM users WHERE usuario = '$Usuario'";
+$sql1 = "SELECT Login FROM users WHERE Login = '$Usuario'";
 $buscar_user = mysqli_query($conn,$sql1);
 $total = mysqli_num_rows($buscar_user);
-echo ($total);
+
 
 if(($total) > 0 )
 {
+	echo ($total);
     echo 'Usuário já existe';
 
 }
 
-$sql = "SELECT email FROM users WHERE email = '$Email'";
+$sql = "SELECT Email FROM users WHERE Email = '$Email'";
 $buscar_email = mysqli_query($conn,$sql);
 $total1 = mysqli_num_rows($buscar_email);
 
-if(($total1) > 0){
+if(($total1) > 0)
+{
 	echo 'Email já existe';
 }
 else{
-	$sql = "INSERT INTO users(usuario, email, senha) VALUES ('$Usuario', '$Email', '$Senha',$Grupo_Id)";
+	$sql = "INSERT INTO users(Login, Email, Senha, Grupo_Id) VALUES ('$Usuario', '$Email', '$Senha','$Grupo_Id')";
+	if ($conn->query($sql) === TRUE)
+	{
+		echo 'Usuário cadastrado com sucesso!';
+	}
+	else
+	{
+		echo "Error: " . $sql . "<br>" . $conn->error;
+	}
 	
-	$conn->query($sql);
-	echo 'Usuário cadastrado com sucesso!';
+	
 }
 /*if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
